62999cb1cab6959b9d06ecaaee91d627bb59f216
danish636 <124681722+danish636@users.noreply.github.com>
2025-08-09 00:03:37 +0530

    Update settings.py

--- stock_project/settings.py
+++ stock_project/settings.py
@@ -160,7 +160,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #"apiapp.middleware.EncryptionMiddleware",
+    "apiapp.middleware.EncryptionMiddleware",
 ]
 
 
